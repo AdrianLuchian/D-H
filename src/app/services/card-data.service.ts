13454efb3f3661a3749f907bb4b16690921fbe72
Adrian Luchian <adrian.luchian@up-co.com>
2025-05-11 13:53:54 +0300

    cardDataService: reactive data store via behaviour subject

--- src/app/services/card-data.service.ts
+++ src/app/services/card-data.service.ts
@@ -1,9 +1,36 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+import { CardData, CardStatus } from '../models/card-data.interface';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class CardDataService {
+  private cardData = new BehaviorSubject<CardData>({
+    title: 'Title',
+    subtitle: 'Subtitle',
+    counter: 0,
+    containerTitle: 'Title Container',
+    status: 'closed',
+    active: false,
+  });
 
-  constructor() { }
+  cardData$ = this.cardData.asObservable();
+
+  updateCardData(data: Partial<CardData>) {
+    const currentData = this.cardData.value;
+    this.cardData.next({ ...currentData, ...data });
+  }
+
+  toggleStatus() {
+    const currentData = this.cardData.value;
+    const newStatus: CardStatus =
+      currentData.status === 'open' ? 'closed' : 'open';
+    this.updateCardData({ status: newStatus });
+  }
+
+  toggleActiveState() {
+    const currentData = this.cardData.value;
+    this.updateCardData({ active: !currentData.active });
+  }
 }
