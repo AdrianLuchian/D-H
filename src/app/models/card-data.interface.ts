c1b2796e45acd47beb4e754de233b2e5ede98652
Adrian Luchian <adrian.luchian@up-co.com>
2025-05-11 13:44:49 +0300

    add models: CardStatus type + CardData interface
    add counter

---
+++ src/app/models/card-data.interface.ts
@@ -0,0 +1,10 @@
+export type CardStatus = 'open' | 'closed';
+
+export interface CardData {
+    title: string;
+    subtitle: string;
+    counter: number;
+    containerTitle: string;
+    status: CardStatus;
+    active: boolean;
+}
